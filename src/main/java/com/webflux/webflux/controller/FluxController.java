a8008e5bc0493d19ca35508dd7666b555c659c26
Sudeep-cv <basicsinjava@gmail.com>
2019-08-11 16:14:39 +0000

    generate

--- src/main/java/com/webflux/webflux/controller/FluxController.java
+++ src/main/java/com/webflux/webflux/controller/FluxController.java
@@ -1,5 +1,6 @@
 package com.webflux.webflux.controller;
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,28 @@ import reactor.core.publisher.Flux;
 @RestController
 class FluxController{
 
-    @GetMapping(value="/fluxtest",produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    @GetMapping(value="/",produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<String> testFlux(){
         return Flux.just("string1","String2","string3").delayElements(Duration.ofSeconds(1));
     }
 
+    @GetMapping(value="/generate",produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<String> testFluxGenerate(){
+
+
+Flux<String> flux = Flux.generate(
+    AtomicLong::new,
+      (state, sink) -> {
+      long i = state.getAndIncrement();
+      sink.next("3 x " + i + " = " + 3*i);
+      if (i == 10) sink.complete();
+      return state;
+    }, (state) -> System.out.println("state: " + state));
+
+
+    return flux;
+
+
+
+    }
 }
